f8ec6c64510908ad2259c2973ffdc48a354bf7ba
Ricardo Pinto <hessian_loom_0u@icloud.com>
2024-06-24 14:04:57 +0100

    Changes to lualine

--- dotfiles/nvim/.config/nvim/lua/plugins/lualine.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -15,6 +15,18 @@ return {
           "neo-tree"
         }
       }
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch" },
+      lualine_c = {
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0.5 } },
+        { "filename", path = 1,         padding = 0,    fmt = function(str) return string.format('%%#Bold#%s', str) end }
+      },
+      lualine_x = {
+        { "encoding", separator = "", padding = 0 },
+        { "filetype", separator = "" }
+      }
     }
   }
 }
