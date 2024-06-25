6554876f3ddefa39ad7f302e72e80e401cd6978f
Ricardo Pinto <hessian_loom_0u@icloud.com>
2024-06-25 15:19:42 +0100

    Changed nvim lualine

--- dotfiles/nvim/.config/nvim/lua/plugins/lualine.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -18,7 +18,7 @@ return {
     },
     sections = {
       lualine_a = { "mode" },
-      lualine_b = { "branch" },
+      lualine_b = { { "branch", color = { gui = "bold" } } },
       lualine_c = {
         { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0.5 } },
         { "filename", path = 1,         padding = 0,    fmt = function(str) return string.format('%%#Bold#%s', str) end }
