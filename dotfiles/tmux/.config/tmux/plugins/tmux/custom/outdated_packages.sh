bcf96608dd34b4aac06759edf2e02fe5e21aa195
Ricardo Pinto <hessian_loom_0u@icloud.com>
2024-06-25 13:15:44 +0100

    Changed tmux outdated packages icon

--- dotfiles/tmux/.config/tmux/plugins/tmux/custom/outdated_packages.sh
+++ dotfiles/tmux/.config/tmux/plugins/tmux/custom/outdated_packages.sh
@@ -2,7 +2,7 @@ show_outdated_packages() {
   local index icon color text module
 
   index=$1
-  icon="$(get_tmux_option "@catppuccin_outdated_packages_icon" "")"
+  icon="$(get_tmux_option "@catppuccin_outdated_packages_icon" "")"
   color="$(get_tmux_option "@catppuccin_outdated_packages_color" "$thm_magenta")"
   text="$(get_tmux_option "@catppuccin_outdated_packages_text" "#(cat ~/.config/tmux/custom/brew-outdated) • #(cat ~/.config/tmux/custom/mise-outdated)")"
 
