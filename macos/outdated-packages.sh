7e3e28e3477b59002f93765f0b74179787598f64
Ricardo Pinto <hessian_loom_0u@icloud.com>
2024-06-28 09:56:02 +0100

    Refactored cron scripts to unload and load

--- macos/outdated-packages.sh
+++ macos/outdated-packages.sh
@@ -6,30 +6,14 @@ function main() {
   cd cron-scripts
   /opt/homebrew/bin/stow outdated-packages -t ${HOME}/Library/LaunchAgents
   cd ..
-  if ! launchctl list | grep -q "com.user.brewoutdatedpackages"; then
-    launchctl load ~/Library/LaunchAgents/com.user.brewoutdatedpackages.plist
-  else
-    launchctl unload ~/Library/LaunchAgents/com.user.brewoutdatedpackages.plist
-    launchctl load ~/Library/LaunchAgents/com.user.brewoutdatedpackages.plist
-  fi
-  if ! launchctl list | grep -q "com.user.miseoutdatedpackages"; then
-    launchctl load ~/Library/LaunchAgents/com.user.miseoutdatedpackages.plist
-  else
-    launchctl unload ~/Library/LaunchAgents/com.user.miseoutdatedpackages.plist
-    launchctl load ~/Library/LaunchAgents/com.user.miseoutdatedpackages.plist
-  fi
-  if ! launchctl list | grep -q "com.user.tmuxoutdatedpackages"; then
-    launchctl load ~/Library/LaunchAgents/com.user.tmuxoutdatedpackages.plist
-  else
-    launchctl unload ~/Library/LaunchAgents/com.user.tmuxoutdatedpackages.plist
-    launchctl load ~/Library/LaunchAgents/com.user.tmuxoutdatedpackages.plist
-  fi
-  if ! launchctl list | grep -q "com.user.nvimoutdatedpackages"; then
-    launchctl load ~/Library/LaunchAgents/com.user.nvimoutdatedpackages.plist
-  else
-    launchctl unload ~/Library/LaunchAgents/com.user.nvimoutdatedpackages.plist
-    launchctl load ~/Library/LaunchAgents/com.user.nvimoutdatedpackages.plist
-  fi
+  launchctl unload ~/Library/LaunchAgents/com.user.brewoutdatedpackages.plist > /dev/null
+  launchctl load ~/Library/LaunchAgents/com.user.brewoutdatedpackages.plist
+  launchctl unload ~/Library/LaunchAgents/com.user.miseoutdatedpackages.plist > /dev/null
+  launchctl load ~/Library/LaunchAgents/com.user.miseoutdatedpackages.plist
+  launchctl unload ~/Library/LaunchAgents/com.user.tmuxoutdatedpackages.plist > /dev/null
+  launchctl load ~/Library/LaunchAgents/com.user.tmuxoutdatedpackages.plist
+  launchctl unload ~/Library/LaunchAgents/com.user.nvimoutdatedpackages.plist > /dev/null
+  launchctl load ~/Library/LaunchAgents/com.user.nvimoutdatedpackages.plist
 }
 
 main
