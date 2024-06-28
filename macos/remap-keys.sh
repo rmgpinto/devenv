2e1d49fe13f11f5ae573b4ee3f5062ccc8560a74
Ricardo Pinto <hessian_loom_0u@icloud.com>
2024-06-28 09:56:02 +0100

    Refactored cron scripts to unload and load

--- macos/remap-keys.sh
+++ macos/remap-keys.sh
@@ -5,12 +5,8 @@ function main() {
   cd cron-scripts
   /opt/homebrew/bin/stow remap-capslock-to-control -t ${HOME}/Library/LaunchAgents
   cd ..
-  if ! launchctl list | grep -q "com.user.remapcapslocktocontrol"; then
-    launchctl load ~/Library/LaunchAgents/com.user.remapcapslocktocontrol.plist
-  else
-    launchctl unload ~/Library/LaunchAgents/com.user.remapcapslocktocontrol.plist
-    launchctl load ~/Library/LaunchAgents/com.user.remapcapslocktocontrol.plist
-  fi
+  launchctl unload ~/Library/LaunchAgents/com.user.remapcapslocktocontrol.plist > /dev/null
+  launchctl load ~/Library/LaunchAgents/com.user.remapcapslocktocontrol.plist
 }
 
 main
