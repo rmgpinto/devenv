8a9d73ae05a945f2d213e6d4b9b3cb6822c58a9d
Ricardo Pinto <hessian_loom_0u@icloud.com>
2024-06-28 08:20:25 +0100

    Added functions to cron scripts

--- macos/cron-scripts/outdated-packages/bin/mise-outdated-packages.sh
+++ macos/cron-scripts/outdated-packages/bin/mise-outdated-packages.sh
@@ -1,7 +1,15 @@
 #!/bin/zsh -eo pipefail
 
-if [ -f /tmp/miseoutdatedpackages.log ]; then
-  echo "" > /tmp/miseoutdatedpackages.log
-fi
-/opt/homebrew/bin/mise plugins update && /opt/homebrew/bin/mise outdated | tail -n +2 | wc -l | tr -d ' ' > ~/.config/tmux/custom/mise-outdated
+function create_log_file() {
+  if [ -f /tmp/miseoutdatedpackages.log ]; then
+    echo "" > /tmp/miseoutdatedpackages.log
+  fi
+}
+
+function main() {
+  create_log_file
+  /opt/homebrew/bin/mise plugins update && /opt/homebrew/bin/mise outdated | tail -n +2 | wc -l | tr -d ' ' > ~/.config/tmux/custom/mise-outdated
+}
+
+main
 
--- macos/cron-scripts/outdated-packages/bin/tmux-outdated-packages.sh
+++ macos/cron-scripts/outdated-packages/bin/tmux-outdated-packages.sh
@@ -1,8 +1,16 @@
 #!/bin/zsh -eo pipefail
 
-if [ -f /tmp/tmuxoutdatedpackages.log ]; then
-  echo "" > /tmp/tmuxoutdatedpackages.log
-fi
-PATH=$PATH:~/.local/share/mise/shims
-~/.config/tmux/plugins/tpm/bin/update_plugins all
+function create_log_file() {
+  if [ -f /tmp/tmuxoutdatedpackages.log ]; then
+    echo "" > /tmp/tmuxoutdatedpackages.log
+  fi
+}
+
+function main() {
+  create_log_file
+  PATH=$PATH:~/.local/share/mise/shims
+  ~/.config/tmux/plugins/tpm/bin/update_plugins all
+}
+
+main
 
