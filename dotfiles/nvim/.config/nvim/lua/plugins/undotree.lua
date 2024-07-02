aab341f3579f0f9dd5d6d255cb78919ffb1bffe5
Ricardo Pinto <hessian_loom_0u@icloud.com>
2024-07-02 23:07:13 +0100

    Changed undotree mappings to be silent

--- dotfiles/nvim/.config/nvim/lua/plugins/undotree.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/undotree.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     vim.g.undotree_WindowLayout = 3
     vim.g.undotree_SetFocusWhenToggle = 1
-    vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>", {})
-    vim.keymap.set("n", "<leader>fu", ":UndotreeToggle<CR>", {})
+    vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>", { silent = true })
+    vim.keymap.set("n", "<leader>fu", ":UndotreeToggle<CR>", { silent = true })
   end
 }
