65a6960796302a9be060b30f7c756fac0460d143
Ricardo Pinto <hessian_loom_0u@icloud.com>
2024-07-09 17:03:56 +0100

    Changed neo-tree to show last part of path

--- dotfiles/nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -9,6 +9,17 @@ return {
     vim.keymap.set("n", "<leader>b", ":Neotree filesystem toggle left<CR>", { silent = true })
     require("neo-tree").setup({
       filesystem = {
+        components = {
+          -- only show last part of path
+          name = function(config, node, state)
+            local components = require('neo-tree.sources.common.components')
+            local name = components.name(config, node, state)
+            if node:get_depth() == 1 then
+              name.text = name.text:match("([^/]+)$")
+            end
+            return name
+          end,
+        },
         hijack_netrw_behavior = "open_default",
         use_libuv_file_watcher = true,
         follow_current_file = {
