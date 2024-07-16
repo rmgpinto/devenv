f98e27c94056f7a2e2ce49a46b6c083f029555a2
Ricardo Pinto <hessian_loom_0u@icloud.com>
2024-07-16 09:21:41 +0100

    Refactored neovim completions

--- dotfiles/nvim/.config/nvim/lua/plugins/completions.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/completions.lua
@@ -5,7 +5,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     config = function()
-      local cmp = require 'cmp'
+      local cmp = require "cmp"
       cmp.setup({
         snippet = {
           expand = function(args)
@@ -17,16 +17,16 @@ return {
           documentation = cmp.config.window.bordered()
         },
         mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true })
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true })
         }),
         sources = cmp.config.sources({
-          { name = 'nvim_lsp' }
+          { name = "nvim_lsp" }
         }, {
-          { name = 'buffer' }
+          { name = "buffer" }
         })
       })
     end
