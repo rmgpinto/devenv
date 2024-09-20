63f68c0bac3582f3612ccd147340146cb4afcc7f
Ricardo Pinto <hessian_loom_0u@icloud.com>
2024-09-20 11:45:25 +0100

    Changed mini pairs neovim config

--- dotfiles/nvim/.config/nvim/lua/plugins/mini-pairs.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/mini-pairs.lua
@@ -1,6 +1,18 @@
 return {
   "echasnovski/mini.pairs",
   config = function()
-    require("mini.pairs").setup({})
+    require("mini.pairs").setup({
+      mappings = {
+        ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\][%s%)%]%}]" },
+        ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\][%s%)%]%}]" },
+        ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\][%s%)%]%}]" },
+        [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+        ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+        ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+        ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^%w][^%w]", register = { cr = false } },
+        ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%w][^%w]", register = { cr = false } },
+        ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^%w][^%w]", register = { cr = false } },
+      }
+    })
   end
 }
