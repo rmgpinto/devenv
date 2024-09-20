a66bd5b512baa1c3beb427f9bbce01c717d42de2
Ricardo Pinto <hessian_loom_0u@icloud.com>
2024-09-20 15:07:46 +0100

    Fixed none-ls neovim config

--- dotfiles/nvim/.config/nvim/lua/plugins/none-ls.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
-      source = {
+      sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.shfmt,
         null_ls.builtins.formatting.terraform_fmt,
