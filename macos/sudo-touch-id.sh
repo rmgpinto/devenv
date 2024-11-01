2c1fbecbc28690624ed2f2d2cf7f3aebe1dc6315
Ricardo Pinto <hessian_loom_0u@icloud.com>
2024-11-01 15:54:22 +0000

    Removed comment

--- macos/sudo-touch-id.sh
+++ macos/sudo-touch-id.sh
@@ -2,7 +2,7 @@
 
 function main() {
   sudo tee /etc/pam.d/sudo_local >/dev/null <<EOF
-# Enable TouchID for sudo in tmux
+# Enable TouchID for sudo
 auth       optional       /opt/homebrew/Cellar/pam-reattach/1.3/lib/pam/pam_reattach.so ignore_ssh
 # Enable TouchID for sudo
 auth       sufficient     pam_tid.so
