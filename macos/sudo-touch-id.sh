1932ec6aefee5b5ef7d562b77f3916d1614de89a
Ricardo Pinto <hessian_loom_0u@icloud.com>
2025-01-10 11:03:54 +0000

    Removed pam-reattach

--- macos/sudo-touch-id.sh
+++ macos/sudo-touch-id.sh
@@ -3,8 +3,6 @@
 function main() {
   sudo tee /etc/pam.d/sudo_local >/dev/null <<EOF
 # Enable TouchID for sudo
-auth       optional       /opt/homebrew/Cellar/pam-reattach/1.3/lib/pam/pam_reattach.so ignore_ssh
-# Enable TouchID for sudo
 auth       sufficient     pam_tid.so
 EOF
 }
