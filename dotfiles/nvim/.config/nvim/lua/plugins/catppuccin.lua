96d145c11027cbbc1b7f0594e7918ccf3278333f
Ricardo Pinto <hessian_loom_0u@icloud.com>
2025-04-17 11:31:42 +0100

    Updated neovim theme

--- dotfiles/nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -6,8 +6,23 @@ return {
   config = function()
     require("catppuccin").setup({
       integrations = {
+        copilot_vim = true,
+        dap = true,
+        dap_ui = true,
+        gitsigns = true,
         mason = true,
-      },
+        native_lsp = {
+          enabled = true,
+        },
+        treesitter = true,
+        telescope = {
+          enabled = true
+        },
+        snacks = {
+          enabled = true
+        },
+        which_key = true
+      }
     })
     vim.cmd.colorscheme("catppuccin-mocha")
     -- Highlight String to teal
