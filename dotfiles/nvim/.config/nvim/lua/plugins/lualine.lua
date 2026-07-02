eec84bc9873f83883c036718edad4fb7f5019c36
Ricardo Pinto <hessian_loom_0u@icloud.com>
2026-07-02 12:39:40 +0100

    Changed branch icon

--- dotfiles/nvim/.config/nvim/lua/plugins/lualine.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -9,7 +9,7 @@ return {
     },
     sections = {
       lualine_a = { "mode" },
-      lualine_b = { { "branch", color = { gui = "bold" } } },
+      lualine_b = { { "branch", icon = "", color = { gui = "bold" } } },
       lualine_c = {
         { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0.5 } },
         {
