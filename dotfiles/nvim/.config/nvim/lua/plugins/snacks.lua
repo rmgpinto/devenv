f4938d971fcc9c8221cc9e8c801d3948eccb737b
Ricardo Pinto <hessian_loom_0u@icloud.com>
2026-09-23 13:43:57 +0200

    Changed nvim to use gh-latest

--- dotfiles/nvim/.config/nvim/lua/plugins/snacks.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -50,10 +50,10 @@ return {
             {
               icon = " ",
               title = "Open PRs",
-              cmd = "/Users/rmgpinto/.local/share/mise/installs/github-cli/latest/gh-latest pr list -L 3",
+              cmd = "/Users/rmgpinto/.config/gh/gh-latest pr list -L 3",
               key = "p",
               action = function()
-                vim.fn.jobstart("/Users/rmgpinto/.local/share/mise/installs/github-cli/latest/gh-latest pr list --web", { detach = true })
+                vim.fn.jobstart("/Users/rmgpinto/.config/gh/gh-latest pr list --web", { detach = true })
               end,
               height = 7,
             },
